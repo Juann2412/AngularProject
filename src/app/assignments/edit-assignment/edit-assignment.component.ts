e071185b922d2b9911e234c62efc0d48c6d37511
Juannito Ratovoniony <juannitoratovoniony9@gmail.com>
2022-04-29 22:51:21 +0200

    Utilisation d'un formulaire de type stepper(Edit)

--- src/app/assignments/edit-assignment/edit-assignment.component.ts
+++ src/app/assignments/edit-assignment/edit-assignment.component.ts
@@ -4,6 +4,7 @@ import { AssignmentsService } from 'src/app/Shared/assignments.service';
 import { Assignment } from '../assignment.model';
 import { Matiere } from '../matiere.model';
 import { MatieresService } from 'src/app/Shared/matieres.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-assignment',
@@ -15,17 +16,25 @@ export class EditAssignmentComponent implements OnInit {
   nomAssignment!: string;
   dateDeRendu!: Date;
   nomEleve!: string;
-  selectedMatiere : Matiere = new Matiere;
+  selectedMatiere: Matiere = new Matiere;
   matiere: Matiere[] = [];
   idMatiere !: Number;
   noteAssignment!: number;
   remarqueAssignment!: string;
+  firstFormGroup !: FormGroup;
+  secondFormGroup !: FormGroup;
+  thirdFormGroup !: FormGroup;
+  fourthFormGroup !: FormGroup;
+  fifthFormGroup !: FormGroup;
+  sixthFormGroup !: FormGroup;
+  isLinear = false;
 
   constructor(
     private assignmentsService: AssignmentsService,
     private route: ActivatedRoute,
     private router: Router,
-    private matiereService: MatieresService
+    private matiereService: MatieresService,
+    private _formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -53,41 +62,61 @@ export class EditAssignmentComponent implements OnInit {
       this.idMatiere = assignment.matiere;
 
       // Pour pré-remplir le formulaire
-      this.nomAssignment = assignment.nom;
+      /**this.nomAssignment = assignment.nom;
       this.dateDeRendu = assignment.dateDeRendu;
       this.nomEleve = assignment.eleve;
       this.noteAssignment = assignment.note;
       this.remarqueAssignment = assignment.remarque;
+      **/
+
+      this.firstFormGroup = this._formBuilder.group({
+        firstCtrl: ['', Validators.required],
+      });
+      this.secondFormGroup = this._formBuilder.group({
+        assignmentCtrl: [assignment.nom, Validators.required],
+      });
+      this.thirdFormGroup = this._formBuilder.group({
+        eleveCtrl: [assignment.eleve, Validators.required],
+      });
+      this.fourthFormGroup = this._formBuilder.group({
+        dateCtrl: [assignment.dateDeRendu, Validators.required],
+      });
+      this.fifthFormGroup = this._formBuilder.group({
+        noteCtrl: [assignment.note, Validators.required],
+      });
+      this.sixthFormGroup = this._formBuilder.group({
+        remarqueCtrl: [assignment.remarque, Validators.required],
+      });
     });
   }
 
   getMatiere() {
 
-        this.matiereService.getMatiere().subscribe((matiere) => {
-          if (!matiere) return;
+    this.matiereService.getMatiere().subscribe((matiere) => {
+      if (!matiere) return;
 
-          this.matiere = matiere;
+      this.matiere = matiere;
 
-          for (let mat of this.matiere) {
-            if(mat.id == this.idMatiere)
-              this.selectedMatiere = mat;
-        }
-        });
+      for (let mat of this.matiere) {
+        if (mat.id == this.idMatiere)
+          this.selectedMatiere = mat;
       }
+    });
+  }
 
   onSaveAssignment() {
     if (!this.assignment) return;
-    if ((!this.nomAssignment) || (!this.dateDeRendu)) return;
+    if ((!this.secondFormGroup.controls['assignmentCtrl'].value) || (!this.thirdFormGroup.controls['eleveCtrl'].value) || (!this.fourthFormGroup.controls['dateCtrl'].value)) return;
     console.log(
       'nom = ' + this.nomAssignment + ' date de rendu = ' + this.dateDeRendu + ' matiere = ' + this.selectedMatiere.id
     );
 
     // on récupère les valeurs dans le formulaire
-    this.assignment.nom = this.nomAssignment;
-    this.assignment.eleve = this.nomEleve;
-    this.assignment.dateDeRendu = this.dateDeRendu;
-    this.assignment.note = this.noteAssignment;
-    this.assignment.remarque = this.remarqueAssignment;
+    this.assignment.nom = this.secondFormGroup.controls['assignmentCtrl'].value;
+    this.assignment.eleve = this.thirdFormGroup.controls['eleveCtrl'].value;
+    this.assignment.dateDeRendu = this.fourthFormGroup.controls['dateCtrl'].value;
+    this.assignment.note = this.fifthFormGroup.controls['noteCtrl'].value;
+    this.assignment.remarque = this.sixthFormGroup.controls['remarqueCtrl'].value;
     this.assignment.matiere = this.selectedMatiere.id;
 
     this.assignmentsService
